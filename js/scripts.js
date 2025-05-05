eaf6c8d959bb0cb4b11696d400ca5a095b785f1b
samale <andriusvx@gmail.com>
2025-05-05 15:29:09 +0200

    paslepta
    hide viska

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.addEventListener('DOMContentLoaded', function () {
+    const telefoon = '+31' + '633996547'; // Obfuscated format
+    const belKnop = document.createElement('a');
+    belKnop.href = 'tel:' + telefoon;
+    belKnop.className = 'btn btn-light btn-lg';
+    belKnop.innerText = 'Bel ons nu';
+
+    const knopContainer = document.getElementById('telefoon-knop');
+    if (knopContainer) {
+        knopContainer.appendChild(belKnop);
+    }
+});
